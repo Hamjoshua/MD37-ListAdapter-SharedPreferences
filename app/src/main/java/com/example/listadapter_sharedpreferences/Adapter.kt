274d963aa6db819696c9ee370ced106c89087393
Hamjoshua <biryukov-10@list.ru>
2024-11-16 19:27:58 +0700

    База с 5 лабы

---
+++ app/src/main/java/com/example/listadapter_sharedpreferences/Adapter.kt
@@ -0,0 +1,31 @@
+package com.example.listadapter_sharedpreferences
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class CardViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    val nameView : TextView = view.findViewById<TextView>(R.id.r_name);
+    val phoneView : TextView = view.findViewById<TextView>(R.id.r_phone);
+    val typeView : TextView = view.findViewById<TextView>(R.id.r_type);
+}
+
+class CardAdapter(private var list: ArrayList<Contact>) : RecyclerView.Adapter<CardViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.r_item,
+            parent, false);
+        return CardViewHolder(view);
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        val contact = list[position];
+        holder.nameView.setText(contact.name);
+        holder.phoneView.setText(contact.phone);
+        holder.typeView.setText(contact.type);
+    }
+}
--- app/src/main/java/com/example/listadapter_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/listadapter_sharedpreferences/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.listadapter_sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import timber.log.Timber
+import java.net.HttpURLConnection
+import java.net.URL
+
+private var contacts : ArrayList<Contact> = ArrayList<Contact>()
+private var editableContacts : ArrayList<Contact> = ArrayList<Contact>()
 
 class MainActivity : AppCompatActivity() {
+    public lateinit var RView : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+
+        RView = findViewById<RecyclerView>(R.id.r_view);
+        RView.layoutManager = LinearLayoutManager(this)
+
+        val btn: Button = findViewById<Button>(R.id.btn_search);
+        val editText: EditText = findViewById<EditText>(R.id.search_edit);
+
+        CoroutineScope(Dispatchers.IO).launch {
+            var text: String = "";
+            val url: URL = URL("https://drive.google.com/u/0/uc?id=1-KO" +
+                    "-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&export=download")
+            val urlConn: HttpURLConnection = url.openConnection() as HttpURLConnection
+            text = urlConn.inputStream.bufferedReader().readText()
+
+            val gson = Gson();
+            val itemType = object : TypeToken<ArrayList<Contact>>() {}.type
+            contacts = gson.fromJson<ArrayList<Contact>>(text, itemType);
+            editableContacts = contacts.clone() as ArrayList<Contact>
+
+            withContext(Dispatchers.Main) {
+                if(RView.context != null){
+                    RView.adapter = CardAdapter(editableContacts);
+                }
+            }
+        }
+
+        btn.setOnClickListener{
+            val line: String = editText.text.toString()
+            if (line != ""){
+                editableContacts = contacts.filter { it.name.contains(line) ||
+                        it.type.contains(line) ||
+                        it.phone.contains(line) } as ArrayList<Contact>;
+            } else{
+                editableContacts = contacts.clone() as ArrayList<Contact>;
+            }
+            RView.adapter = CardAdapter(editableContacts);
+        }
     }
 }
+
+
+
+data class Contact(
+    val phone: String,
+    val name: String,
+    val type: String
+)
